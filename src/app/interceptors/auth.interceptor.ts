6073afb7a7b91d3b6a8a04bd1b0278b342f02663
breiner-dev <breinerr006@gmail.com>
2026-03-15 21:24:01 -0500

    eliminacion de logs

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -2,13 +2,8 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  //const authService = inject(AuthService);
-  //const token = authService.getToken();
 
   const token = localStorage.getItem('auth_token');
-
-  console.log('INTERCEPTOR TOKEN:', token);
-  console.log('INTERCEPTOR URL:', req.url);
 
   if (!token) {
     return next(req);
@@ -19,8 +14,6 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       Authorization: `Bearer ${token}`,
     },
   });
-
-  console.log('AUTH HEADER ENVIADO:', reqConToken.headers.get('Authorization'));
 
   return next(reqConToken);
 };
